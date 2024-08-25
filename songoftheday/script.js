48911f3c930a21ad4894c7657f424f0f9390beef
3DartBlade <59492834+3DartBlade@users.noreply.github.com>
2024-08-25 08:29:32 +0200

    Update script.js

--- songoftheday/script.js
+++ songoftheday/script.js
@@ -70,7 +70,7 @@ async function fetchVideoByIndex(index) {
     const items = data.items;
     const item = items[videoIndexInPage];
     const snippet = item['snippet'];
-	document.getElementById("para").innerHTML = snippet["title"]; 
+	document.getElementById("para").innerHTML = snippet["title"] + '\n(' + snippet['resourceId']['videoId'] + ')'; 
     const resourceId = snippet['resourceId'];
     const videoId = resourceId['videoId'];
     return videoId;
